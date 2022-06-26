d1b661ceadfde552e937b962b97f114e79e04c15
MESHach12345 <meshachadriansamuel@gmail.com>
2022-06-25 19:10:41 -0700

    Arrays 2 Initial Commit

--- CSE205/Array2/src/Source.java
+++ CSE205/Array2/src/Source.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Source {
     public static void main(String[] args) {
-        
+        int[][] twoD;
+        twoD = new int[10][];
+        for (int y = 0; y<twoD.length; y++) {
+            twoD[y] = new int[10];
+            for (int x = 0; x<twoD[y].length; x++) {
+                System.out.printf("%4d", twoD[y][x]);
+            }
+            System.out.println();
+        }
     }
 }
