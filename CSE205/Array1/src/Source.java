2e2077c0164846dad3413e0837a2f6cf333f2285
MESHach12345 <meshachadriansamuel@gmail.com>
2022-06-25 18:56:30 -0700

    Arrays2 Initial Commit

--- CSE205/Array1/src/Source.java
+++ CSE205/Array1/src/Source.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 
-public class Arrays1 {
+public class Source {
     public static void main(String[] args) {
-        int [] myArray = new int[10];
+        int [] myArray = new int[100];
+        int currentSize = 0;
         char[] randomLetters;
 
         for (int i = 0; i < myArray.length; i++) {
             myArray[i] = (int)(Math.random()*1000);
+            currentSize++;
         }
 
         // System.out.println("");
@@ -24,7 +26,22 @@ public class Arrays1 {
         
         // printArray(randomLetters);
 
-        prettyPrintArray(myArray);
+        // prettyPrintArray(myArray);
+
+        for (int i = 0; i<20; i++) {
+            int delete = (int)(Math.random()*currentSize);
+            System.out.println("Deleting Index: " + delete);
+            currentSize = deleteItem(myArray, currentSize, delete);
+            prettyPrintArray(myArray, currentSize);
+        }
+
+        for (int i = 0; i<20; i++) {
+            int target = (int)(Math.random()*currentSize);
+            int value = (int)(Math.random()*1000);
+            System.out.println("Inserting Index: " + target + " Value: " + value);
+            currentSize = insertItemAt(myArray, currentSize, target, value);
+            prettyPrintArray(myArray, currentSize);
+        }
 
     }
 
@@ -52,6 +69,14 @@ public class Arrays1 {
         }
 
         System.out.print(array[array.length-1]);
+    }
+
+    public static void prettyPrintArray(int[] array, int currentSize) {
+        for (int i = 0; i < currentSize-1; i++) {
+            System.out.print(array[i] + " | ");
+        }
+
+        System.out.print(array[currentSize-1]);
     }
 
     public static void doubleStuff(int[] myArray) {
@@ -110,6 +135,61 @@ public class Arrays1 {
         }
 
         return currentSize;
-    } 
-}
+    }
 
+    public static int linearSeach(int[] myArray, int searchValue) {
+        int pos = 0;
+        while (pos<myArray.length) {
+            if (myArray[pos] == searchValue) {
+                return pos;
+            } else {
+                pos++;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Deleting an item from an Array, shuffling everything up one position
+     * @param array
+     * @param deleteIndex
+     */
+
+    public static int deleteItem(int[] array, int currentSize, int deleteIndex) {
+    //We are deleting at deleteIndex
+    //Everything 'above it' moves "back" one index
+    
+    for (int target = deleteIndex; target< currentSize - 1; target++) {
+        int swap = target+1;
+        array[target] = array[swap];
+    }
+
+    currentSize--;
+    
+    return currentSize;
+    }
+
+    /**
+     * Insert a value into an array
+    * @param array array to insert into
+    * @param currentSize current of valid items in the array
+    * @param insertIndex index to insrt into
+    * @param value value going in
+    * @return new valid item count (currentSize)
+    */
+
+    public static int insertItemAt(int[] array, int currentSize, int insertIndex, int value) {
+    if (currentSize == array.length) {
+        throw new IndexOutOfBoundsException("Arary already Full");
+    }
+    currentSize++;
+
+    for (int target = currentSize - 1; target > insertIndex; target--) {
+        array[target] = array[target-1];
+    }
+
+    array[insertIndex] = value;
+
+    return currentSize;
+    }
+}
