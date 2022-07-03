17deac2a60b6fee002de9417eadc79d086ce3481
MESHach12345 <meshachadriansamuel@gmail.com>
2022-07-03 16:44:05 -0700

    Key Presses

---
+++ JavaScript/Detecting Key Presses/script.js
@@ -0,0 +1,33 @@
+var divEl = document.getElementById("myDiv");
+
+let x=0;
+let y=0;
+
+window.addEventListener("keydown", move);
+
+function move(direction) {
+    switch(direction.key) {
+        case "ArrowDown":
+            y+=5;
+            myDiv.style.top = y+"px";
+            break;
+        
+        case "ArrowUp":
+            y-=5;
+            myDiv.style.top = y+"px";
+            break;
+        
+        case "ArrowLeft":
+            x-=5;
+            myDiv.style.left = x+"px";
+            break;
+
+        case "ArrowRight":
+            x+=5;
+            myDiv.style.left = x+"px";
+            break;
+
+        default: 
+            break;
+    }
+}
