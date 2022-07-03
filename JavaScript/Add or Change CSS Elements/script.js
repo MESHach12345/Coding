67e591d0cd165abc042f8f41f209e275fc37614e
MESHach12345 <meshachadriansamuel@gmail.com>
2022-07-03 13:58:53 -0700

    Adding or Changing CSS Properties

---
+++ JavaScript/Add or Change CSS Elements/script.js
@@ -0,0 +1,8 @@
+var titleEl = document.getElementById("myTitle");
+
+titleEl.style.backgroundColor = "#222222";
+titleEl.style.color = "rgb(50,200,250)";
+titleEl.style.fontFamily = "Consolas";
+titleEl.style.textAlign = "center";
+titleEl.style.border = "2px solid";
+titleEl.style.display = "block";
