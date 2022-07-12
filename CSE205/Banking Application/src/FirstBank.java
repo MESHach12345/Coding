268e8336d3d435c144abab7ed83c47dd9385a529
Meshach Samuel <83641520+MESHach12345@users.noreply.github.com>
2022-07-11 18:32:13 -0700

    Deleted Folders

--- CSE205/Banking Application/src/FirstBank.java
+++
@@ -1,6 +0,0 @@
-public class FirstBank {
-    public static void main(String[] args) {
-        Account tim = new Account("Tim Statler", "A00001");
-        tim.showMenu();
-    }
-}
--- JavaScript/Classes/script.js
+++ JavaScript/Classes/script.js
@@ -1,5 +1,38 @@
 class Car {
-    constructor(name, color, speed) {
-        
+    constructor(name, color, topSpeed) {
+        this.name = name;
+        this.color = color;
+        this.topSpeed = topSpeed;
+        this.currentSpeed = 0;
+    }
+
+    drive() {
+        console.log("Just Drove 2 Miles!");
+        this.curentSpeed += 10;
+        console.log(`Current Speed: ${this.currentSpeed} mph`);
+    }
+
+    brake() {
+        console.log("Just Stopped the Car!");
     }
 }
+
+// let ferrari = new Car("Ferrari", "Red", 250);
+// console.log(ferrari.name);
+// console.log(ferrari.color);
+// console.log(ferrari.topSpeed);
+
+// ferrari.drive();
+// ferrari.brake();
+
+const numbers = [1,2,3];
+number.push(4);
+
+Array.prototype.myPush = function () {
+    return this.length;
+}
+
+const fruits = ['🍌', "🍓", "🍪", "🍐", "🍎"];
+// 
+
+console.log(fruits.myPush);
